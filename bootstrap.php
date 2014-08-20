86c042bb5166a17ba295edc5119d2fdefe7a8377
Muhammad Safraz <mr.safraz@gmail.com>
2014-08-20 20:08:45 +0530

    update configuration

--- bootstrap.php
+++ bootstrap.php
@@ -2,8 +2,11 @@
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
-require_once "vendor/autoload.php";
+$loader = require_once "vendor/autoload.php";
+
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 $isDevMode = true;
 
@@ -12,20 +15,20 @@ $isDevMode = true;
 // if you prefer yaml
 //$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/doctrine/yaml"), $isDevMode);
 // or if you prefer XML
-$config = Setup::createXMLMetadataConfiguration(array(__DIR__ . "/doctrine/xml"), $isDevMode);
+$config = Setup::createXMLMetadataConfiguration(array(__DIR__ . "/config/doctrine"), $isDevMode);
 // database configuration parameters
 $conn = array(
     'driver' => 'pdo_sqlite',
-    'path' => __DIR__ . '/doctrine/db.sqlite',
+    'path' => __DIR__ . '/config/db.sqlite',
 );
 
 // uncomment to use MySql 
 //$conn = array(
 //    'driver' => 'pdo_mysql',
 //    'host' => 'localhost',
-//    'dbname' => 'your_database_name',
-//    'user' => 'username',
-//    'password' => 'password'
+//    'dbname' => 'breeze_server_demo2',
+//    'user' => 'root',
+//    'password' => ''
 //);
 
 // live demo app specific setup:
